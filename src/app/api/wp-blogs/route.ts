53b354a94955dca0790373412311ad166ce26c75
Zuhaib Ulla shariff <zuhaibullashariff7862@gmail.com>
2026-09-11 17:00:30 +0530

    Fix: update WordPress blog proxy endpoint to cms.pentacloud.in

--- src/app/api/wp-blogs/route.ts
+++ src/app/api/wp-blogs/route.ts
@@ -12,11 +12,12 @@ import { NextRequest, NextResponse } from 'next/server';
  */
 
 const ALLOWED_DOMAINS: Record<string, string> = {
-  'pentacloud.in': 'https://pentacloud.in',
+  'pentacloud.in': 'https://cms.pentacloud.in',
+  'cms.pentacloud.in': 'https://cms.pentacloud.in',
   'pentacloudconsulting.com': 'https://pentacloudconsulting.com',
 };
 
-const FALLBACK_BASE = 'https://pentacloud.in';
+const FALLBACK_BASE = 'https://cms.pentacloud.in';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
